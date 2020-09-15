c556db9eb3b075e2cf96e3e3cbd34f65af056c1d
dm-for-git <dm-for-git@protonmail.com>
2020-09-15 15:51:34 +0700

    Reborn answer

---
+++ RotateArray/RotateArray-Reborn.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+func shiftRight(anArray: inout [Int], by distance: Int = 1) {
+    let count = anArray.count
+    let maxIndex = count - 1
+    var index = maxIndex
+    var tempArr = [Int]()
+    while(index >= count - distance){
+        tempArr.append(anArray[index])
+        index -= 1
+    }
+    for i in 0...(maxIndex - distance) {
+        tempArr.append(anArray[i])
+    }
+    anArray = tempArr
+}
+
+func rotateArray(inputArray: inout [Int], by times: Int) {
+    if inputArray.isEmpty || inputArray.count <= times {
+        return
+    }
+    for _ in 0..<times {
+        shiftRight(anArray: &inputArray)
+    }
+}
