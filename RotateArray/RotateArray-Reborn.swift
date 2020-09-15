969fc9cfb4c4cb6eac3149215b62889c66325202
dm-for-git <dm-for-git@protonmail.com>
2020-09-15 22:02:28 +0700

    Fix wrong algorithm

--- RotateArray/RotateArray-Reborn.swift
+++ RotateArray/RotateArray-Reborn.swift
@@ -2,22 +2,15 @@ import Foundation
 
 func shiftRight(anArray: inout [Int], by distance: Int = 1) {
     let count = anArray.count
-    let maxIndex = count - 1
-    var index = maxIndex
-    var tempArr = [Int]()
-    while(index >= count - distance){
-        tempArr.append(anArray[index])
-        index -= 1
-    }
-    for i in 0...(maxIndex - distance) {
-        tempArr.append(anArray[i])
-    }
-    anArray = tempArr
-}
-
-func rotateArray(inputArray: inout [Int], by times: Int) {
-    if inputArray.isEmpty || inputArray.count <= times {
+    if count == 0 || count <= distance || distance < 1 {
         return
     }
-    shiftRight(anArray: &inputArray, by: times)
+    var tempArray = [Int]()
+    for i in (count-distance)..<count {
+        tempArray.append(anArray[i])
+    }
+    for j in 0..<(count-distance) {
+        tempArray.append(anArray[j])
+    }
+    anArray = tempArray
 }
